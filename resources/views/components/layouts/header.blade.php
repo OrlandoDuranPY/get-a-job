6c1e5848a2cde8d766e7d07dceba9cf17383e1c2
OrlandoDuranPY <orlandoduranpy@outlook.es>
2024-09-17 23:59:03 -0600

    Estilos para header

--- resources/views/components/layouts/header.blade.php
+++ resources/views/components/layouts/header.blade.php
@@ -5,16 +5,17 @@
     <div class="container mx-auto flex justify-between items-center relative">
         <img src="{{ asset('img/logo.svg') }}" class="h-14" alt="Logo">
         {{-- Menu --}}
-        <nav class="absolute left-1/2 transform -translate-x-1/2">
+        <nav class="absolute left-1/2 transform -translate-x-1/2 text-gray-600">
             <ul class="flex space-x-6">
                 <li>
-                    <a href="#" class="hover:text-blue-500">Home</a>
+                    <a href="/"
+                        class="font-semibold @if (Route::is('home')) text-blue-500 @else hover:text-blue-500 @endif">Inicio</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:text-blue-500">Vacantes</a>
+                    <a href="#" class="font-semibold hover:text-blue-500">Vacantes</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:text-blue-500">Curriculums</a>
+                    <a href="#" class="font-semibold hover:text-blue-500">Curriculums</a>
                 </li>
             </ul>
         </nav>
@@ -22,10 +23,10 @@
         <nav>
             <ul class="flex space-x-6">
                 <li>
-                    <a href="#" class="hover:text-blue-500">Login</a>
+                    <a href="#" class="font-semibold hover:text-blue-500">Login</a>
                 </li>
                 <li>
-                    <a href="#" class="hover:text-blue-500">Register</a>
+                    <a href="#" class="font-semibold hover:text-blue-500">Register</a>
                 </li>
             </ul>
         </nav>
